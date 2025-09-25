da35be6a266379ca9c100dd27032c743bb64f7ed
nyasha <nyashazee07@gmail.com>
2025-09-25 12:35:15 +0200

    Initial commit

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -34,11 +34,11 @@ export default function Footer() {
         <div>
           <FH1>Get In Touch With Us</FH1>
           <ul className="space-y-1 text-[9px] md:text-base text-[white]">
-            <li className="flex items-center gap-2">
+            <li className="flex items-center gap-1 md:gap-2">
               <Phone className="w-2 h-2 md:w-4 md:h-4 text-white" />
               08080151
             </li>
-            <li className="flex items-center gap-2">
+            <li className="flex items-center gap-1 md:gap-2">
               <Phone className="w-2 h-2 md:w-4 md:h-4 text-white" />
               08080152
             </li>
