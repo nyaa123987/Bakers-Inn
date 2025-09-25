f03fdbc82a0326ac8c07512bd822288b77d49887
nyasha <nyashazee07@gmail.com>
2025-09-25 13:20:55 +0200

    Initial commit

--- src/pages/components/KidsHero.tsx
+++ src/pages/components/KidsHero.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const KidsHero = () => {
     return(
-        <main className='bg-white pl-[10%] pb-[5vh] md:pb-[15vh]'>
+        <main className='bg-white pl-[6%] md:pl-[10%] pb-[5vh] md:pb-[15vh] pt-[2vh] md:pt-0'>
             <Image src="/images/kids-picture.png" alt="Kids' Section" width={1150} height={900} />
         </main>
     )
--- src/pages/components/SpaceshipMaze.tsx
+++ src/pages/components/SpaceshipMaze.tsx
@@ -1,15 +1,14 @@
 import Image from 'next/image';
 import Button from './Buttons';
 import H2 from './Heading2';
-import P from './Paragraph';
 
 const SpaceshipMaze = () => {
     return(
         <main className='bg-[#018BF1] flex flex-col text-center md:text-start md:flex-row md:h-[80vh] px-[3%] py-[2vh] md:my-[6vh] gap-[15%] justify-between align-middle'>
             <div className='flex flex-col justify-center'>
                 <H2 className='text-white'>Spaceship Maze</H2>
-                <P className='pb-[2vh] md:pt-[4vh] text-white text-[15px] md:text-[20px]'>Help the spaceship through the maze to get back to earth. Move through levels with increasing difficulty; each puzzle starts the ship in a different location.</P>
-                <P className='pb-[2vh] md:pb-[4vh] text-white text-[15px] md:text-[20px]'>Develop kids’ skills in strategizing, spatial awareness, hand-eye coordination, problem solving and more! Free to play, priceless for learning.</P>
+                <p className='pb-[2vh] md:pt-[4vh] text-white text-[12px] md:text-[20px]'>Help the spaceship through the maze to get back to earth. Move through levels with increasing difficulty; each puzzle starts the ship in a different location.</p>
+                <p className='pb-[2vh] md:pb-[4vh] text-white text-[12px] md:text-[20px]'>Develop kids’ skills in strategizing, spatial awareness, hand-eye coordination, problem solving and more! Free to play, priceless for learning.</p>
                 <div>
                     <a href="https://toytheater.com/maze/" target="_blank" rel="noopener noreferrer">
                         <Button>play game</Button>
@@ -17,7 +16,7 @@ const SpaceshipMaze = () => {
                 </div>
             </div>
             <div className='flex justify-center align-middle p-[5vh]'>
-                <Image src="/images/Maze.png" alt="Spaceship maze" width={1500} height={1000} />
+                <Image src="/images/Maze.png" alt="Spaceship maze" width={1500} height={1000} className='w-25 md:w-200' />
             </div>
         </main>
     )
