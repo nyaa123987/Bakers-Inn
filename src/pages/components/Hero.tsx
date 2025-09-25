70ad762ac28ce76e16cac0dce0973d84f5448543
nyasha <nyashazee07@gmail.com>
2025-09-25 11:08:33 +0200

    Initial commit

--- src/pages/components/Hero.tsx
+++ src/pages/components/Hero.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 export default function Hero() {
     return(
         <main>
-            <section className="flex w-[100%] h-[60vh] md:h-[80vh] justify-center items-center bg-[#261B6C] text-[white]">
+            <section className="flex w-[100%] h-[50vh] md:h-[80vh] justify-center items-center bg-[#261B6C] text-[white]">
                 <div className="pl-[3%] md:w-[60%]">
                     <H1 className="text-xl md:text-[60px]">Bringing you the best value at the best prices.</H1>
                     <P className="text-white text-base md:text-[22px] pb-[3vh]">The freshest sandwiches you can make are with Baker’s inn soft white loaf.</P>
